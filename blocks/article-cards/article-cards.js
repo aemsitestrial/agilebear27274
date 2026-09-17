cfd5628fccf09ddfdedc3a502e41af61ede34481
Mohammed V S <2963721@tcs.com>
2026-09-17 15:32:41 +0530

    modified

--- blocks/article-cards/article-cards.js
+++ blocks/article-cards/article-cards.js
@@ -1,42 +1,100 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 import { moveInstrumentation } from '../../scripts/scripts.js';
 
+function textOf(div) {
+  return div ? div.textContent.trim() : '';
+}
+
+function buildMetaItem(div, className, tag = 'span') {
+  const el = document.createElement(tag);
+  el.className = className;
+  if (div) {
+    el.textContent = textOf(div);
+    moveInstrumentation(div, el);
+  }
+  return el;
+}
+
 export default function decorate(block) {
   const ul = document.createElement('ul');
 
   [...block.children].forEach((row) => {
-    const li = document.createElement('li');
+    const [
+      imageDiv,
+      tagDiv,
+      categoryDiv,
+      readingTimeDiv,
+      publishedDateDiv,
+      titleDiv,
+      descriptionDiv,
+      authorImageDiv,
+      authorNameDiv,
+      authorRoleDiv,
+    ] = [...row.children];
 
+    const li = document.createElement('li');
     moveInstrumentation(row, li);
 
-    while (row.firstElementChild) {
-      li.append(row.firstElementChild);
+    const imageWrapper = document.createElement('div');
+    imageWrapper.className = 'article-card-image';
+    if (imageDiv) {
+      moveInstrumentation(imageDiv, imageWrapper);
+      while (imageDiv.firstElementChild) imageWrapper.append(imageDiv.firstElementChild);
     }
 
-    [...li.children].forEach((div, index) => {
-      if (index === 0 && div.querySelector('picture')) {
-        div.className = 'article-card-image';
-      } else {
-        div.className = 'article-card-body';
-      }
-    });
+    const meta = document.createElement('div');
+    meta.className = 'article-card-meta';
+    meta.append(
+      buildMetaItem(tagDiv, 'article-card-tag'),
+      buildMetaItem(categoryDiv, 'article-card-category'),
+      buildMetaItem(readingTimeDiv, 'article-card-reading-time'),
+    );
 
+    const published = buildMetaItem(publishedDateDiv, 'article-card-published', 'p');
+    const title = buildMetaItem(titleDiv, 'article-card-title', 'h2');
+
+    const description = document.createElement('div');
+    description.className = 'article-card-description';
+    if (descriptionDiv) {
+      moveInstrumentation(descriptionDiv, description);
+      while (descriptionDiv.firstElementChild) description.append(descriptionDiv.firstElementChild);
+    }
+
+    const authorImage = document.createElement('div');
+    authorImage.className = 'article-card-author-image';
+    if (authorImageDiv) {
+      moveInstrumentation(authorImageDiv, authorImage);
+      while (authorImageDiv.firstElementChild) authorImage.append(authorImageDiv.firstElementChild);
+    }
+
+    const authorInfo = document.createElement('div');
+    authorInfo.className = 'article-card-author-info';
+    authorInfo.append(
+      buildMetaItem(authorNameDiv, 'article-card-author-name', 'p'),
+      buildMetaItem(authorRoleDiv, 'article-card-author-role', 'p'),
+    );
+
+    const author = document.createElement('div');
+    author.className = 'article-card-author';
+    author.append(authorImage, authorInfo);
+
+    const body = document.createElement('div');
+    body.className = 'article-card-body';
+    body.append(meta, published, title, description, author);
+
+    li.append(imageWrapper, body);
     ul.append(li);
   });
 
-  ul.querySelectorAll('picture > img').forEach((img) => {
-    const optimizedPicture = createOptimizedPicture(
-      img.src,
-      img.alt,
-      false,
-      [{ width: '1200' }],
-    );
+  ul.querySelectorAll('.article-card-image picture > img').forEach((img) => {
+    const optimizedPicture = createOptimizedPicture(img.src, img.alt, false, [{ width: '1200' }]);
+    moveInstrumentation(img, optimizedPicture.querySelector('img'));
+    img.closest('picture').replaceWith(optimizedPicture);
+  });
 
-    moveInstrumentation(
-      img,
-      optimizedPicture.querySelector('img'),
-    );
-
+  ul.querySelectorAll('.article-card-author-image picture > img').forEach((img) => {
+    const optimizedPicture = createOptimizedPicture(img.src, img.alt, false, [{ width: '96' }]);
+    moveInstrumentation(img, optimizedPicture.querySelector('img'));
     img.closest('picture').replaceWith(optimizedPicture);
   });
 
